d15c0ba6d5665498da32f38a2f6ce6a35271afb7
Ritesh Biswas <lusebiswas@gmail.com>
2025-02-20 16:33:15 +0530

    Sub Departments Are made

--- home/models.py
+++ home/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel,MultipleChooserPanel
+from django.contrib.auth.models import User
+from modelcluster.fields import ParentalManyToManyField
+from django import forms 
 
 
 class HomePage(Page):
@@ -19,7 +22,33 @@ class DepartmentPage(Page):
     ]
 
     template = 'home/department_page.html'
+    subpage_types = ['home.SubDepartmentPage']
 
     class Meta:
         verbose_name = "Department"
         verbose_name_plural = "Departments"
+
+
+class SubDepartmentPage(Page):
+    name = models.CharField(max_length=100)
+    description = RichTextField(blank=True)
+    site_admins = ParentalManyToManyField(
+        User,
+        related_name='administered_subdepartments',
+        blank=True,
+        limit_choices_to={'is_superuser': False}
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('description'),
+        FieldPanel('site_admins', widget=forms.CheckboxSelectMultiple),
+    ]
+
+    template = 'home/sub_department_page.html'
+    parent_page_types = ['home.DepartmentPage']
+    subpage_types = []  # No child pages allowed
+
+    class Meta:
+        verbose_name = "Sub Department"
+        verbose_name_plural = "Sub Departments"
